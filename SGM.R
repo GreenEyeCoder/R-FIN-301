2e7ea506185a2a2c5d80e77d92f24467f5077878
Joel DeRouchey <55360086+GreenEyeCoder@users.noreply.github.com>
2022-11-06 00:17:49 -0700

    updated the percison

--- SGM.R
+++ SGM.R
@@ -3,20 +3,20 @@ SGM <- function(D0,g,t,cg,ce){
   for(i in 1:t ){
     if (i == 1){
       D = round(D0*(1+g),4)
-      cat(sprintf("D%i = %0.4f X (1 + %0.2f) = %0.4f\n",i,D0,g,D))
+      cat(sprintf("D%i = %0.4f X (1 + %0.3f) = %0.4f\n",i,D0,g,D))
       }
     else{
     D = append(D,round(D[i-1]*(1+g),4))
     }
-    cat(sprintf("D%i = %0.4f X (1 + %0.2f) = %0.4f\n",i,D[i-1],g,D[i]))
+    cat(sprintf("D%i = %0.4f X (1 + %0.3f) = %0.4f\n",i,D[i-1],g,D[i]))
   }
   
   cat(sprintf("Step 2\n"))
   Dt = D[length(D)]*(1+cg)
-  cat(sprintf("Dt = %0.4f X (1    + %0.2f)  =   %0.4f\n",D[length(D)],cg,Dt))
+  cat(sprintf("Dt = %0.4f X (1    + %0.3f)  =   %0.4f\n",D[length(D)],cg,Dt))
   
   Vt = round(Dt/(ce-cg),4)
-  cat(sprintf("Vt = %0.4f / (%0.2f - %0.2f)  =  %0.4f\n",Dt,ce,cg,Vt))
+  cat(sprintf("Vt = %0.4f / (%0.3f - %0.3f)  =  %0.4f\n",Dt,ce,cg,Vt))
   for (i in 1:t){
     if(i == 1 ){
       V0 = D[1]/(1+ce)^i
@@ -28,9 +28,9 @@ SGM <- function(D0,g,t,cg,ce){
   V0 = round(Vt/(1+ce)^t + V0,2)
   cat(sprintf("Step 3\n"))
   for(i in 1:t-1){
-    cat(sprintf("%0.4f / (1 + %0.2f)^%i + ",D[i],ce,i))
+    cat(sprintf("%0.4f / (1 + %0.3f)^%i + ",D[i],ce,i))
   }
-    cat(sprintf("(%0.4f + %0.4f) / (1 + %0.2f)^%i = %0.2f\n",D[t],Vt,ce,t,V0))
+    cat(sprintf("(%0.4f + %0.4f) / (1 + %0.3f)^%i = %0.3f\n",D[t],Vt,ce,t,V0))
  }
 
 SGM(1.45,0.15,4,0.06,0.11)
